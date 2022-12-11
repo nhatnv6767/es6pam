1ea868cf503aa8acba452ec08d3eb74c4538f0cb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-12 01:08:29 +0900

    object literal

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 let programs = ["nothing", "else", "capcom"]
 let [first, second, third] = programs
+
+
+// Object literal
+
+let name = "ten";
+let myObj = {
+    // ten: "nothing"
+    [name]: "another thing",
+    sayHi() {
+        console.log("Hello, my name is " + this.name)
+    }
+}
+
+myObj.sayHi()
