d6c7cf0d8b23a4114ee5c44c02f97772642f9d78
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-12 22:22:08 +0900

    ung dung thu kinh 7.1

--- js/thukinh/glassesList.js
+++ js/thukinh/glassesList.js
@@ -12,13 +12,15 @@ export class GlassesList {
         let content = ""
         // dung reduce de co lenh return, nem' gia tri ra ben ngoai
         // glContent: giá trị return ra khỏi vòng lặp sau khi đã hoàn thành
-        this.glist.reduce((glContent, item, index) => {
-            glContent = `
+        content = this.glist.reduce((glContent, item, index) => {
+            glContent += `
             <div class="col-4">
-                <img src="${item.src}" alt="Glasses">
+                <img class="img-fluid" src="${item.src}" alt="Glasses">
             </div>
             `
+            return glContent;
         })
+        return content;
     }
 
 }
