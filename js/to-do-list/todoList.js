bcf78180452b8affd2c94cfffab8561cffea2e41
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-14 22:24:02 +0900

    todolist 8 render to do content

--- js/to-do-list/todoList.js
+++ js/to-do-list/todoList.js
@@ -10,8 +10,20 @@ export class ToDoList {
     renderToDo() {
         let content = ""
         this.tdList.reduceRight((tdContent, item, index) => {
-
-        })
+            tdContent += `
+                <li>
+                    <span>${item.textTodo}</span>
+                    <div class="buttons">
+                        <button class="remove">
+                            <i class="fa fa-trash-alt"></i>
+                        </button>
+                        <button class="complete">
+                            <i class="far fa-check-circle"></i>
+                        </button>
+                    </div>
+                </li>
+            `
+        }, "")
     }
 
 }
